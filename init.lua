1c1202d2540a7a619bb15c8de6e8d7c7b81bca80
Duc Thanh Vu <73021414+thanhvd1403@users.noreply.github.com>
2026-05-16 14:56:19 +0700

    Update init.lua

--- init.lua
+++ init.lua
@@ -12,3 +12,9 @@ vim.api.nvim_set_hl(0, "LineNr", { ctermfg = 8 })
 
 -- Right-align line numbers in status column
 vim.opt.statuscolumn = "%=%{v:relnum == 0 ? v:lnum : v:relnum} "
+
+-- Set tab to 2 spaces
+vim.opt.tabstop = 2      -- Number of spaces that a <Tab> in the file counts for
+vim.opt.softtabstop = 2  -- Number of spaces that a <Tab> counts for while performing editing operations
+vim.opt.shiftwidth = 2   -- Number of spaces to use for each step of (auto)indent
+vim.opt.expandtab = true -- Use spaces instead of tabs
